322f41c2e6fe0d51331386dbc6edbc6c3ba42e95
Pikulinaha <146559827+Pikulinaha@users.noreply.github.com>
2025-10-14 22:40:23 +0300

    Add files via upload
    Урок ItProger 8-Словари

---
+++ 8.py
@@ -0,0 +1,32 @@
+#country = {(5,6): 3} #----5,6 ключ, 3- значение
+#print(country[5, 6])
+from urllib.response import addbase
+
+#country = {'code':'RU', 'Name': 'Russian', 'populatiom':  144}
+
+#country = dict(code='RU', name='Russian') #Alternative
+#print(country['code'])
+
+#for key, value in country.items(): #key-доступ к ключам, items-значениям
+#   print(key, " - ",value)
+#country.clear()#очистка
+#country.pop('Name')#delete for key
+#country.popitem()# delete last element
+#print(country.items()) #keys+value
+#print(country.get('Name'))# alternative []
+#country['code']='none' #можно обновлять значения
+#print(country)
+
+#person = {
+ #   'user_1':{
+ #       'first_name': 'John',
+ #       'last_name': 'Marley',
+  #      'age':45,
+ #       'address':['sity. Moscow', 'street Orchard road', '45'],
+ #       'grades': {'math': 5, 'english':3}
+ #   },
+ #   'user_2':{
+
+ #   }
+#}
+#print(person['user_1']['address'][1]) #Тут пример как обращаться к нужному элементу словаря
