1a1ecd216193c25935c67f96ec4bea0db37a589f
Pikulinaha <146559827+Pikulinaha@users.noreply.github.com>
2025-10-16 20:49:55 +0300

    #12 Функции def and lambda

---
+++ 12.py
@@ -0,0 +1,43 @@
+#Функции def and lambda-можно создавать что угодно и сколько угодно
+#def test_func(word):             #word-параметр
+ #   #pass            #pass-ничего
+  #  print(word, end="")
+   # print("!")
+#test_func("Hi")
+#def summa(a,b):
+ #   return a + b #return получить данные после выполнения функции
+#res=summa(5,7)
+#print(res)
+#print(summa("H","i"))
+
+# nums2=[5.4,7.2,2.3,2.1,9.4,4.2]
+# min2=nums2[0]
+# for el in nums2:
+#     if el < min2:
+#         min2=el
+# print(min2)
+
+# def minimal(l):        это сокращенный код который выше через def
+#     min_number=l[0]
+#     for el in l:
+#         if el<min_number:
+#             min_number=el
+#     return min_number
+#
+#
+# nums1=[5,7,2,9,4]
+# min1=minimal(nums1)
+#
+# nums2=[5.4,7.2,2.3,2.1,1.9,9.4,4.2]
+# min2=minimal(nums2)
+#
+# if min1<min2:
+#     print(min1)
+# else:
+#     print(min2)
+# def summa(x,y):
+#     return x*y
+#
+# func=lambda x,y:x*y
+# res=func(5,2)
+# print(res)
